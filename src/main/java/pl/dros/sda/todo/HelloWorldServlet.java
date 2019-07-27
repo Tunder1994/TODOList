74fd40f969a84dcea0378fb4f374d6c9d514e22f
Dawid Rosa <rosadawid94@gmail.com>
2019-07-27 13:28:39 +0200

    New Branch

--- src/main/java/pl/dros/sda/todo/HelloWorldServlet.java
+++ src/main/java/pl/dros/sda/todo/HelloWorldServlet.java
@@ -11,21 +11,21 @@ import java.io.PrintWriter;
 @WebServlet(name = "HelloWorld", urlPatterns = "/world")
 public class HelloWorldServlet extends HttpServlet {
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.append("<html>");
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    PrintWriter writer = resp.getWriter();
+    writer.append("<html>");
 
-        writer.append("<head>");
-        writer.append("<title>Hello World :)</title>");
-        writer.append("</head>");
+    writer.append("<head>");
+    writer.append("<title>Hello World :)</title>");
+    writer.append("</head>");
 
-        writer.append("<body>");
-        writer.append("<h1>Hello World :)</h1>");
-        writer.append("<p>Nowo dodany akapit</p>");
-        writer.append("</body>");
+    writer.append("<body>");
+    writer.append("<h1>Hello World :)</h1>");
+    writer.append("<p>Nowo dodany akapit</p>");
+    writer.append("</body>");
 
-        writer.append("</html>");
-    }
+    writer.append("</html>");
+  }
 }
